0d9a1246e3b1c9674a8e8bef70d6c1b187f01fc4
Zerozany <691139128@qq.com>
2026-02-27 19:36:42 +0800

    update:导入了libusb库

--- cmake/import3rd.cmake
+++ cmake/import3rd.cmake
@@ -1,6 +1,13 @@
 find_package(spdlog CONFIG REQUIRED)
 
+find_library(LIBUSB_1_LIB NAMES usb-1.0)
+
+if(NOT LIBUSB_1_LIB)
+    message(FATAL_ERROR "libusb-1.0 library not found")
+endif()
+
 target_link_libraries(${PROJECT_NAME}
     PUBLIC
     spdlog::spdlog
+    ${LIBUSB_1_LIB}
 )
